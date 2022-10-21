a3416c01d3d897fc80dee8ad4c60a352e38ee78b
AGASTYA12don <108648725+AGASTYA12don@users.noreply.github.com>
2022-10-21 17:50:05 +0545

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,21 @@
+// javaScript Variables
+//container to store data values
+// var number1 = 10;
+// var number2 = 120;
+// var number3 = 20;
+// console.log(number1 + number2);
+
+// var str1 = "  This is a string";
+// var str2 = 'This is also a string';
+ 
+// var marks = {
+//     manish: 100,
+//     subham: 33,
+//     happy: 22
+// }
+// console.log(number3);
+
+// console.log(str1);
+// console.log(str2);
+// console.log(marks);
+// console.log(number3 + str1);
