ad76962b2c9a0534f1b4cde6f111c63ddd34011e
AGASTYA12don <108648725+AGASTYA12don@users.noreply.github.com>
2022-10-21 17:47:13 +0545

    Add files via upload

---
+++ booleans.js
@@ -0,0 +1,11 @@
+// //Booleans
+// var a = true;
+// var b = false;
+// console.log(a,b)
+
+// var und = undefined;
+// console.log(undefined);
+// console.log(und);
+// var und;
+// var n = null;
+// console.log(n);
