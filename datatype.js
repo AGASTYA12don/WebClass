0f19ccbcc93db8121a5975bfca9d0a1dad92f4d5
AGASTYA12don <108648725+AGASTYA12don@users.noreply.github.com>
2022-10-21 17:48:25 +0545

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,33 @@
+//Data types in java Script
+//numbers
+// var number3 = 55;
+// var number4 = 60;
+// //strings
+// var str1 = "  This is a string";
+// var str2 = 'This is also a string';
+ //Objects
+// var marks = {
+//     manish: 100,
+//     subham: 33,
+//     happy: 22
+// }
+// console.log(number3);
+// console.log(number4);
+// console.log(str1);
+// console.log(str2);
+// console.log(marks);
+// console.log(number3 + str1);
+
+
+//at a very high leval, there are two types of 
+// data types in JavaScript
+
+//1.Primitive data  types: Undefined, null, number, string, booleans, symbol,
+//2.reference data types: arrays and objects
+
+// var arr = [1,2,"maish",3,4,5]
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
