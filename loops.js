cc5714d85a3091f8e32cdee7962bb1a4305dce67
AGASTYA12don <108648725+AGASTYA12don@users.noreply.github.com>
2022-10-21 17:48:44 +0545

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,71 @@
+//var arr = [1, 2, 3, 4, 5, 6];
+// console.log(arr);
+// for(var i=0;i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(element){
+//     console.log(element)
+// })
+
+//let j = 2;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j ++;
+// }
+
+// do {
+// console.log(arr[j]);
+// j++
+// }while(j<arr.length);
+
+// for (var i=0;i<arr.length;i++){
+//     if(i==2){
+//         //break;
+//         continue;
+//     }
+
+//     console.log(arr[i])
+// }
+
+
+//let myArr = ["man",4, "camera", 34, null, true];
+//Arrey Methods
+//console.log(myArr.length)
+
+//myArr.pop();
+//myArr.push("manish");
+//myArr.shift();
+//myArr.unshift("manish ");
+//onsole.log(myArr);
+
+
+
+//string method in java script
+
+//let mylovelystring = "Happpy birthday ";
+// console.log(mylovelystring.length)
+// console.log(mylovelystring.indexOf("Happy"))
+// console.log(mylovelystring.lastindexOf("Happy"));
+//console.log(mylovelystring.slice(0,5));
+//d = mylovelystring.replace("birthday", "Manish");
+//console.groupCollapsed(d, mylovelystring);
+
+
+
+let myDate = new Date();
+console.log(myDate);
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+
+
+
+//DOM Manuplation
+// let em = document.getElementById('click');
+// console.log(em);
+
+// let elemClass = document.getElementsByClassName("cointainer");
+// console.log(elemClass);
+//elemClass[0].style.background = "yellow";
+//elemClass[0].classList.add("br-primary")
+
