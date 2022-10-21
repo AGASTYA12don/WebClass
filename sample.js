35d44a49e26390e6a75e8d75aada9f139bd76ee2
AGASTYA12don <108648725+AGASTYA12don@users.noreply.github.com>
2022-10-21 17:49:49 +0545

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,94 @@
+//1.Ways to print in javascript
+//console.log("Hello world");
+//alert("me");
+//document.write("THis is a document write")
+
+//2. Javascript console API
+// console.log("Hello world", 4 + 4, "MAnish Malla");
+// console.warn("This is a warning");
+
+
+//Events in JavaScript
+
+
+// function clicked(){
+//     console.log('The button was clicked')
+// }
+
+// window.onload = function(){
+//     console.log('The Document was loaded')
+// }
+
+
+// firstcointainer.addEventListener('click',function(){
+
+//     console.log("clicker on cointainer")
+// })
+// firstcointainer.addEventListener('mouseover',function(){
+
+//     console.log("mouse over the cointainer")
+// })
+// firstcointainer.addEventListener('mouseout',function(){
+
+//     console.log("mouse out cointainer")
+//})
+// firstcointainer.addEventListener('mouseup',function(){
+
+//     console.log("mouse on when clicked on cointainer")
+// })
+// firstcointainer.addEventListener('mousedown',function(){
+
+//     console.log("mouse down when clicked on cointainer")
+// })
+
+
+//arrow function
+// function sum(a, b){
+//     return a+b;
+// }
+// summ = (a,b)=>{
+//     return a+b;
+// }
+
+
+//
+//srtTime out function
+
+// setInterval(login, 2000);
+
+
+//local Storage in Java Script
+
+// localStorage.setItem('name', 'manish')
+// localStorage
+// localStorage.getItem('name')
+// localStorage.removeItem('name')
+// localStorage.clear()
+
+
+//Json 
+// obj = {name: "manish", length:1}
+//  js = JSON.stringify(obj);
+//  console.log(js)
+//  console.log(typeof js)
+// console.log(js)
+
+// parsed = JSON.parse('name: "manish", length:1')
+
+// console.log(parsed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
