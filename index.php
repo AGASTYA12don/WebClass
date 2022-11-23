afdc263ff8137337339e3049932dcb685c895138
Manish_Malla <108648725+AGASTYA12don@users.noreply.github.com>
2022-11-23 11:56:07 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Single Line Comment
+
+/* This is a Double line comment
+This should need to be known to all of the fphp programmer. */
+echo "<h1> Hello from Manish Bikram Malla.";
+$x="<h1>This is a variable x<h1>";
+$y="This is a variable y";
+echo $x.$y
+?>
